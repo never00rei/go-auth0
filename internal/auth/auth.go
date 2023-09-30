d43f9a59f4cf0cbbd8c49db6f922690a9643c76d
never00rei <jwashington104.jw@gmail.com>
2023-09-30 13:49:38 +0100

    feat: working token endpoint

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,20 +1,81 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/never00rei/go-auth0/internal/config"
 )
 
-type Auth0AuthToken struct {
-	ClientAuth         config.ClientAuth
-	OauthToken         string
-	CreatedDate        time.Time
-	ExpiresDate        time.Time
-	RefreshToken       string
-	RefreshExpiresDate time.Time
+type BearerToken struct {
+	OauthToken string `json:"access_token"`
+	TokenType  string `json:"token_type"`
 }
 
-func GetOauthToken(c config.ClientAuth) (string, error) {
-	return "", nil
+type Auth0AuthToken struct {
+	ClientAuth  config.ClientAuth
+	Token       string
+	Type        string
+	CreatedDate time.Time
+	ExpiresDate time.Time
+}
+
+func (a Auth0AuthToken) GetOauthToken(c config.ClientAuth) (string, error) {
+	tokenUrl := fmt.Sprintf("https://%s/oauth/token", c.ApiDomain)
+
+	tokenPayload := strings.NewReader(
+		fmt.Sprintf(
+			`{"client_id":"%s","client_secret":"%s","audience":"https://%s/api/v2/","grant_type":"client_credentials"}`,
+			c.ClientID,
+			c.ClientSecret,
+			c.ApiDomain,
+		),
+	)
+
+	req, err := http.NewRequest("POST", tokenUrl, tokenPayload)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Add("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("failed to get token: %v", res.Status)
+	}
+
+	var bearerToken BearerToken
+
+	if err := json.NewDecoder(res.Body).Decode(&bearerToken); err != nil {
+		return "", fmt.Errorf("failed to decode response: %v", err)
+	}
+
+	a.CreatedDate = time.Now()
+	a.ExpiresDate = a.CreatedDate.Add(time.Second * 86400)
+	a.Token = fmt.Sprintf("%s %s", bearerToken.TokenType, bearerToken.OauthToken)
+
+	log.Println(a.Token)
+
+	return a.Token, nil
+}
+
+func (a Auth0AuthToken) IsTokenExpired() bool {
+	return time.Now().After(a.ExpiresDate)
+}
+
+func (a Auth0AuthToken) RefreshAuthToken(c config.ClientAuth) (string, error) {
+	if time.Now().After(a.ExpiresDate) {
+		return a.GetOauthToken(c)
+	}
+
+	return a.Token, nil
 }
