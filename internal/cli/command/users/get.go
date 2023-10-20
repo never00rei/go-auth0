e26b23026e4910655c02ff1ddd853e80a5241daa
James Washington <jwashington104.jw@gmail.com>
2023-10-20 18:29:02 +0100

    chore: added quick get framework

--- internal/cli/command/users/get.go
+++ internal/cli/command/users/get.go
@@ -1,1 +1,25 @@
 package users
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/never00rei/go-auth0/internal/cli/shell"
+	"github.com/never00rei/go-auth0/internal/config"
+)
+
+const (
+	HEADERS  = "Accept: application/json"
+	ENDPOINT = "/users"
+)
+
+func GetAllUsers() {
+	if shell.CheckEnvVarExists(config.EnvSessionTenant) {
+		fmt.Errorf("Tenant environment variable: %s does not exist, have you logged in?", config.EnvSessionTenant)
+	}
+
+	if shell.CheckEnvVarExists(config.EnvSessionBearerToken) {
+		fmt.Errorf("Token environment variable: %s does not exist, have you logged in?", config.EnvSessionBearerToken)
+	}
+
+}
